1c5a53470f6abdac2cbd5a0614113268b7761e91
Barış ÖZDEMİRCİ <ozdemircibaris@gmail.com>
2020-12-15 15:54:05 +0300

    Initial Commit

---
+++ tembelFit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tembelFit
+//
+//  Created by Baris Ozdemirci on 15.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
